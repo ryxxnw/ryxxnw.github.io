c5b7602545725c521d3610cf1884896dbbbc55c9
Ryan <ryan.wildanitahmid17@gmail.com>
2023-04-24 00:46:44 +0700

    variasiin javascript

--- script.js
+++ script.js
@@ -5,22 +5,38 @@ const buttonMale = document.getElementById('buttonMale')
 
 buttonMale.onclick = function() {
     let namaOrang = prompt('Tulis nama kamu:');
-    if (namaOrang != null) {
-        let konfirmasi = confirm('Fuck you ' + namaOrang + '!');
+    if (namaOrang === '') {
+        alert('tulis nama lu etdah, ulang ulang');
+        return;
+    } else if (namaOrang) {
+        var konfirmasi = confirm('Fuck you ' + namaOrang + '!');
+    } else {
+        alert('yaelah dicancel');
+        return;
     };
 
-    if (konfirmasi = true) {
+    if (konfirmasi != false) {
         alert('hehehe becanda jgn marah yaaaa');
+    } else {
+        alert('becanda bang');
     };
 }
 
 buttonFemale.onclick = function() {
     let namaOrang = prompt('Tulis nama kamu:');
-    if (namaOrang != null) {
-        let konfirmasi = confirm('I love you ' + namaOrang + "<3");
+    if (namaOrang === '') {
+        alert('kamu belum ketik apa-apa, ulang ya');
+        return;
+    } else if (namaOrang) {
+        var konfirmasi = confirm('I love you ' + namaOrang + "<3");
+    } else {
+        alert('yaah dicancel');
+        return;
     };
 
-    if (konfirmasi = true) {
+    if (konfirmasi != false) {
         alert('hehehe becanda jgn baper yaaaa');
+    } else {
+        alert('iya, engga');
     };
 }
